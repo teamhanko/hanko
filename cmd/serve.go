0e0094bf0af301d19665ed722a1af807e95a243a
Felix Dubrownik <fdubrownik@gmail.com>
2022-03-15 10:54:34 +0100

    feat: add stub for serve and migrate commands

---
+++ cmd/serve.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2022 Hanko GmbH <developers@hanko.io>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start the hanko server",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serve called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+}
