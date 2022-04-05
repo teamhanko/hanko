eff062b7df44696c3e9b34d02775169134335dcc
Frederic Jahn <frederic.jahn@hanko.io>
2022-04-05 14:35:49 +0200

    fix: add user_id to webauthn_credential

--- persistence/models/webauthn_credential.go
+++ persistence/models/webauthn_credential.go
@@ -11,6 +11,7 @@ import (
 // WebauthnCredential is used by pop to map your webauthn_credentials database table to your go code.
 type WebauthnCredential struct {
 	ID              string    `db:"id"`
+	UserId          uuid.UUID `db:"user_id"`
 	PublicKey       string    `db:"public_key"`
 	AttestationType string    `db:"attestation_type"`
 	AAGUID          uuid.UUID `db:"aaguid"`
@@ -23,6 +24,7 @@ type WebauthnCredential struct {
 func (credential *WebauthnCredential) Validate(tx *pop.Connection) (*validate.Errors, error) {
 	return validate.Validate(
 		&validators.StringIsPresent{Name: "ID", Field: credential.ID},
+		&validators.UUIDIsPresent{Name: "UserId", Field: credential.UserId},
 		&validators.StringIsPresent{Name: "PublicKey", Field: credential.PublicKey},
 		&validators.IntIsGreaterThan{Name: "SignCount", Field: credential.SignCount, Compared: -1},
 		&validators.TimeIsPresent{Name: "CreatedAt", Field: credential.CreatedAt},
