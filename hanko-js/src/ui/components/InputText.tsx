836d12c02480eb9433d72b674d520520d6eaadd5
Björn Müller <bjoern.mueller@hanko.io>
2022-06-13 18:43:06 +0200

    feat(hanko-js): Login and registration without mouse clicks, autofocus individual buttons

--- hanko-js/src/ui/components/InputText.tsx
+++ hanko-js/src/ui/components/InputText.tsx
@@ -1,62 +1,27 @@
 import * as preact from "preact";
+import { h } from "preact";
 import { useEffect, useRef } from "preact/compat";
 
 import styles from "./Input.module.css";
 
-type Props = {
-  name: string;
-  type: string;
-  value?: string;
-  required?: boolean;
-  disabled?: boolean;
-  label: string;
-  onInput: (event: Event) => void;
-  autofocus?: boolean;
-  minLength?: number;
-  maxLength?: number;
-  pattern?: string;
-};
+interface Props extends h.JSX.HTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
 
-const InputText = ({
-  name,
-  type,
-  value,
-  required,
-  disabled,
-  label,
-  onInput,
-  autofocus,
-  minLength,
-  maxLength,
-  pattern,
-}: Props) => {
+const InputText = ({ label, ...props }: Props) => {
   const ref = useRef(null);
 
   useEffect(() => {
     const { current: element } = ref;
-    if (element && autofocus) {
+    if (element && props.autofocus) {
       element.focus();
       element.select();
     }
-  }, [autofocus, disabled]);
+  }, [props.autofocus]);
 
   return (
     <div className={styles.inputWrapper}>
-      <input
-        ref={ref}
-        name={name}
-        type={type}
-        required={required}
-        disabled={disabled}
-        onInput={onInput}
-        value={value}
-        className={styles.input}
-        placeholder={" "}
-        autofocus={autofocus}
-        minLength={minLength}
-        maxLength={maxLength}
-        pattern={pattern}
-      />
+      <input {...props} className={styles.input} />
       <label className={styles.label}>{label}</label>
     </div>
   );
