8b6a6074d2e49a90e0fe741c9ee21b57331b3473
Felix Dubrownik <fdubrownik@gmail.com>
2022-06-17 13:59:04 +0200

    fix: move the las " into the correct position

--- backend/server/middleware/logger.go
+++ backend/server/middleware/logger.go
@@ -10,6 +10,6 @@ func GetLoggerMiddleware() echo.MiddlewareFunc {
 		Format: `{"time":"${time_rfc3339_nano}","time_unix":"${time_unix}","id":"${id}","remote_ip":"${remote_ip}",` +
 			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
 			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"` +
-			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out},"referer":"${referer}}"` + "\n",
+			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out},"referer":"${referer}"}` + "\n",
 	})
 }
