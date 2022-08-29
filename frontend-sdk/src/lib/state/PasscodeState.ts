932233eee88ddc12ebbb912d156ed43fcf5c247a
bjoern-m <56024829+bjoern-m@users.noreply.github.com>
2022-08-29 13:30:47 +0200

    Update frontend-sdk/src/lib/state/PasscodeState.ts
    Co-authored-by: lfleischmann <67686424+lfleischmann@users.noreply.github.com>

--- frontend-sdk/src/lib/state/PasscodeState.ts
+++ frontend-sdk/src/lib/state/PasscodeState.ts
@@ -122,7 +122,7 @@ class PasscodeState extends UserState {
    * Sets the number of seconds until a new passcode can be sent.
    *
    * @param {string} userID - The UUID of the user.
-   * @param {string} seconds - Number of seconds the passcode is valid.
+   * @param {string} seconds - Number of seconds the passcode is valid for.
    * @return {PasscodeState}
    */
   setResendAfter(userID: string, seconds: number): PasscodeState {
