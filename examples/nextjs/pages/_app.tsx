c15327b9af41fd7d15004396f86f00f6c8e83378
Björn Müller <bjoern.mueller@hanko.io>
2022-09-20 15:12:10 +0200

    feat(examples): add favicons

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/index.css";
+import Head from 'next/head';
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Hanko Next.js Example</title>
+      <link rel="icon" href="favicon.png" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp;
