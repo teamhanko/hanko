b450fb505a35be92f0f2f00da2fb37081f67a2c4
lfleischmann <67686424+lfleischmann@users.noreply.github.com>
2022-10-10 02:17:41 -0700

    docs: add plausible analytics (#274)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,6 +13,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  scripts: [{ src: 'https://plausible.io/js/plausible.js', defer: true, 'data-domain': 'docs.hanko.io' }],
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
