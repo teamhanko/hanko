ee4dc4bf8f1b08c37c5f9ea0f3cdbc8d2bf97720
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-08 10:08:47 +0530

    feat(remix-eg): add redirect after auth

--- examples/remix/app/routes/index.tsx
+++ examples/remix/app/routes/index.tsx
@@ -3,17 +3,28 @@ import { ClientOnly } from "remix-utils";
 import { registerHankoAuth } from "~/lib/hanko.client";
 import { Hanko } from "@teamhanko/hanko-frontend-sdk";
 import styles from "~/styles/todo.css";
-import type { LinksFunction } from "@remix-run/node";
+import type { ActionArgs, LinksFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { useFetcher } from "@remix-run/react";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
 
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+  console.log(Object.fromEntries(formData.entries()));
+  return redirect("/todo");
+};
+
 export default function Index() {
+  const fetcher = useFetcher();
+
   const handler = async () => {
     const hanko = new Hanko(window.ENV.HANKO_URL);
     const user = await hanko.user.getCurrent();
-    console.log(user);
+    const data = { hankoId: user.id, email: user.email };
+    fetcher.submit(data, { method: "post" });
   };
 
   useEffect(() => {
