64d7ddda9c7173e1b9c0d9abb08eebe5a051d1bf
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-17 06:14:54 +0530

    ci: run built app in docker container

--- examples/remix/remix.config.js
+++ examples/remix/remix.config.js
@@ -1,8 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  devServerPort: 8008,
 };
