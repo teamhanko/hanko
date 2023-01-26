b52f6db76bf33b74e2b776be3381758f037b63aa
Frederic Jahn <frederic.jahn@hanko.io>
2023-01-26 11:27:44 +0100

    feat: add query params to search users

--- backend/handler/user_admin.go
+++ backend/handler/user_admin.go
@@ -5,11 +5,13 @@ import (
 	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/teamhanko/hanko/backend/dto"
+	"github.com/teamhanko/hanko/backend/dto/admin"
 	"github.com/teamhanko/hanko/backend/pagination"
 	"github.com/teamhanko/hanko/backend/persistence"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 )
 
 type UserHandlerAdmin struct {
@@ -41,18 +43,15 @@ func (h *UserHandlerAdmin) Delete(c echo.Context) error {
 		return fmt.Errorf("failed to delete user: %w", err)
 	}
 
-	return c.JSON(http.StatusNoContent, nil)
-}
-
-type UserPatchRequest struct {
-	UserId   string `param:"id" validate:"required,uuid4"`
-	Email    string `json:"email" validate:"omitempty,email"`
-	Verified *bool  `json:"verified"`
+	return c.NoContent(http.StatusNoContent)
 }
 
 type UserListRequest struct {
-	PerPage int `query:"per_page"`
-	Page    int `query:"page"`
+	PerPage int    `query:"per_page"`
+	Page    int    `query:"page"`
+	Q       string `query:"q"`
+	Email   string `query:"email"`
+	UserId  string `query:"user_id"`
 }
 
 func (h *UserHandlerAdmin) List(c echo.Context) error {
@@ -70,12 +69,23 @@ func (h *UserHandlerAdmin) List(c echo.Context) error {
 		request.PerPage = 20
 	}
 
-	users, err := h.persister.GetUserPersister().List(request.Page, request.PerPage)
+	userId := uuid.Nil
+	if request.UserId != "" {
+		userId, err = uuid.FromString(request.UserId)
+		if err != nil {
+			return dto.NewHTTPError(http.StatusBadRequest, "failed to parse user_id as uuid").SetInternal(err)
+		}
+	}
+
+	email := strings.ToLower(request.Email)
+	q := strings.ToLower(request.Q)
+
+	users, err := h.persister.GetUserPersister().List(request.Page, request.PerPage, q, userId, email)
 	if err != nil {
 		return fmt.Errorf("failed to get list of users: %w", err)
 	}
 
-	userCount, err := h.persister.GetUserPersister().Count()
+	userCount, err := h.persister.GetUserPersister().Count(q, userId, email)
 	if err != nil {
 		return fmt.Errorf("failed to get total count of users: %w", err)
 	}
@@ -85,5 +95,29 @@ func (h *UserHandlerAdmin) List(c echo.Context) error {
 	c.Response().Header().Set("Link", pagination.CreateHeader(u, userCount, request.Page, request.PerPage))
 	c.Response().Header().Set("X-Total-Count", strconv.FormatInt(int64(userCount), 10))
 
-	return c.JSON(http.StatusOK, users)
+	l := make([]admin.User, len(users))
+	for i := range users {
+		l[i] = admin.FromUserModel(users[i])
+	}
+
+	return c.JSON(http.StatusOK, l)
+}
+
+func (h *UserHandlerAdmin) Get(c echo.Context) error {
+	userId, err := uuid.FromString(c.Param("id"))
+	if err != nil {
+		return dto.NewHTTPError(http.StatusBadRequest, "failed to parse userId as uuid").SetInternal(err)
+	}
+
+	p := h.persister.GetUserPersister()
+	user, err := p.Get(userId)
+	if err != nil {
+		return fmt.Errorf("failed to get user: %w", err)
+	}
+
+	if user == nil {
+		return dto.NewHTTPError(http.StatusNotFound, "user not found")
+	}
+
+	return c.JSON(http.StatusOK, admin.FromUserModel(*user))
 }
