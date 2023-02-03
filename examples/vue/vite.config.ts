b513593ff9d3e74d1f44fb469634b39097225889
Felix Dubrownik <fdubrownik@gmail.com>
2023-02-03 11:45:34 +0100

    fix: vite config for vue. now works with workspace setup

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -5,6 +5,12 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ['@teamhanko/hanko-elements'],
+  },
+  build: {
+    commonjsOptions: { include: ['@teamhanko/hanko-elements'] },
+  },
   plugins: [
     vue({
       template: {
