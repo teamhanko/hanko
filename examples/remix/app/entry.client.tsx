b552ce589d391219a2f2f73860c277bcf2b7f102
Carl Youngblood <carl@blockscale.net>
2023-02-09 18:02:23 -0700

    Workaround for facebook/react/issues/24430

--- examples/remix/app/entry.client.tsx
+++ examples/remix/app/entry.client.tsx
@@ -1,22 +1,17 @@
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { hydrate } from "react-dom";
 
-function hydrate() {
+function _hydrate() {
   startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <RemixBrowser />
-      </StrictMode>
-    );
+    hydrate(<StrictMode><RemixBrowser /></StrictMode>, document);
   });
 }
 
 if (window.requestIdleCallback) {
-  window.requestIdleCallback(hydrate);
+  window.requestIdleCallback(_hydrate);
 } else {
   // Safari doesn't support requestIdleCallback
   // https://caniuse.com/requestidlecallback
-  window.setTimeout(hydrate, 1);
+  window.setTimeout(_hydrate, 1);
 }
