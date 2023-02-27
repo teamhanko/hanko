54698ebff9e726046292bb28539bd75b1077b5b3
Björn Müller <bjoern.mueller@hanko.io>
2023-02-27 17:02:18 +0100

    fix: use correct api urlsÂ

--- examples/react/src/HankoProfile.tsx
+++ examples/react/src/HankoProfile.tsx
@@ -4,11 +4,12 @@ import styles from "./Todo.module.css";
 import {useNavigate} from "react-router-dom";
 import {TodoClient} from "./TodoClient";
 
-const api = process.env.REACT_APP_HANKO_API!;
+const hankoApi = process.env.REACT_APP_HANKO_API!
+const todoApi = process.env.REACT_APP_TODO_API!
 
 function HankoProfile() {
   const navigate = useNavigate();
-  const client = useMemo(() => new TodoClient(api), []);
+  const client = useMemo(() => new TodoClient(todoApi), []);
   const [error, setError] = useState<Error | null>(null);
 
   const logout = () => {
@@ -37,15 +38,18 @@ function HankoProfile() {
         <button onClick={logout} className={styles.button}>
           Logout
         </button>
+        <button disabled className={styles.button}>
+          Profile
+        </button>
         <button onClick={todo} className={styles.button}>
-          Todo
+          Todos
         </button>
       </nav>
-    <div className={styles.content}>
-      <h1 className={styles.headline}>Profile</h1>
-      <div className={styles.error}>{error?.message}</div>
-      <hanko-profile api={api} />
-    </div>
+      <div className={styles.content}>
+        <h1 className={styles.headline}>Profile</h1>
+        <div className={styles.error}>{error?.message}</div>
+        <hanko-profile api={hankoApi} />
+      </div>
     </>
   );
 }
