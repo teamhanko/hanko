5f75001df8701664b804ea764aa7d2e1960e806e
Lennart Fleischmann <lennart.fleischmann@hanko.io>
2023-03-01 18:12:10 +0100

    fix(elements): use correct exclamation mark icon

--- frontend/elements/src/components/icons/ExclamationMark.tsx
+++ frontend/elements/src/components/icons/ExclamationMark.tsx
@@ -17,8 +17,7 @@ const ExclamationMark = ({ size, secondary, disabled }: IconProps) => {
         disabled && styles.disabled
       )}
     >
-      <path d="M0 0h24v24H0z" fill="none" />
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>
     </svg>
   );
 };
