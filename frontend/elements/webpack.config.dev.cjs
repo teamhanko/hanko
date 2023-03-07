0accd874afddd0f7396cb19898d634b6f2054db0
Lennart Fleischmann <lennart.fleischmann@hanko.io>
2023-03-07 14:09:36 +0100

    fix(frontend): make compose debug work again
    - Add turbo and top level npm script for building the "dev"/debug version
      (i.e. plus sourcemaps) of the elements package
    - Adjust Dockerfile to use said script
    - Simplify elements webpack "dev"/debug config to make differences to base
      config explicit

--- frontend/elements/webpack.config.dev.cjs
+++ frontend/elements/webpack.config.dev.cjs
@@ -1,77 +1,12 @@
-const path = require("path");
+const baseConfig = require("./webpack.config.cjs");
+
+baseConfig.module.rules.push({
+  test: /\.c?js$/,
+  enforce: "pre",
+  use: ["source-map-loader"],
+})
 
 module.exports = {
   devtool: 'eval-source-map',
-  entry: {
-    elements: {
-      filename: 'elements.js',
-      import: './src/index.ts',
-      library: {
-        name: 'Elements',
-        type: 'umd',
-        umdNamedDefine: true,
-      },
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.c?js$/,
-        enforce: "pre",
-        use: ["source-map-loader"],
-      },
-      {
-        test: /\.(tsx?)$/,
-        use: 'ts-loader',
-        exclude: [/node_modules/, /dist/],
-        resolve: {
-          fullySpecified: false
-        },
-      },
-      {
-        test: /\.(sass)$/,
-        use: [
-          {
-            loader: "style-loader",
-            options: {
-              injectType: 'singletonStyleTag',
-              insert: (styleTag) => {
-                // eslint-disable-next-line no-underscore-dangle
-                window._hankoStyle = styleTag;
-              },
-            },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: "hanko_[local]",
-                localIdentContext: path.resolve(__dirname, "src"),
-              },
-              importLoaders: 1,
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true,
-            }
-          }
-        ]
-      },
-    ]
-  },
-  resolve: {
-    extensions: [
-      '.ts',
-      '.tsx',
-      '.js',
-      '.sass',
-      "declarations.d.ts"
-    ],
-  },
-  output: {
-    clean: true,
-    path: path.resolve(__dirname, 'dist'),
-  },
+  ...baseConfig
 };
