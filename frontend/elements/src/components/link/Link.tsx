c49f26c186113f63d75d9db5e08f68fe9c852029
Björn Müller <bjoern.mueller@hanko.io>
2023-03-14 18:55:21 +0100

    feat: dangerous links require confirmation

--- frontend/elements/src/components/link/Link.tsx
+++ frontend/elements/src/components/link/Link.tsx
@@ -7,12 +7,14 @@ import LoadingSpinner, {
 } from "../icons/LoadingSpinner";
 
 import styles from "./styles.sass";
+import { useCallback, useState } from "preact/compat";
 
 type LoadingSpinnerPosition = "left" | "right";
 
 export interface Props
   extends LoadingSpinnerProps,
     h.JSX.HTMLAttributes<HTMLButtonElement> {
+  onClick(event: Event): void;
   dangerous?: boolean;
   loadingSpinnerPosition?: LoadingSpinnerPosition;
 }
@@ -20,43 +22,78 @@ export interface Props
 const Link = ({
   loadingSpinnerPosition,
   dangerous = false,
+  onClick,
   ...props
 }: Props) => {
-  const renderLink = () => (
-    <button
-      {...props}
-      // @ts-ignore
-      part={"link"}
-      className={cx(
-        styles.link,
-        props.disabled ? styles.disabled : dangerous ? styles.danger : null
-      )}
-    >
-      {props.children}
-    </button>
+  const [confirmationActive, setConfirmationActive] = useState<boolean>();
+
+  const dangerousOnClick = (event: Event) => {
+    event.preventDefault();
+    setConfirmationActive(true);
+  };
+
+  const onCancel = (event: Event) => {
+    event.preventDefault();
+    setConfirmationActive(false);
+  };
+
+  const onConfirmation = useCallback(
+    (event: Event) => {
+      event.preventDefault();
+      setConfirmationActive(false);
+      onClick(event);
+    },
+    [onClick]
+  );
+
+  const renderLink = useCallback(
+    () => (
+      <Fragment>
+        {confirmationActive ? (
+          <Fragment>
+            <Link onClick={onConfirmation}>✓ yes</Link>&nbsp;
+            <Link onClick={onCancel}>✗ no</Link>
+            &nbsp;
+          </Fragment>
+        ) : null}
+        <button
+          {...props}
+          onClick={dangerous ? dangerousOnClick : onClick}
+          disabled={confirmationActive || props.disabled || props.isLoading}
+          // @ts-ignore
+          part={"link"}
+          className={cx(styles.link, dangerous ? styles.danger : null)}
+        >
+          {props.children}
+        </button>
+      </Fragment>
+    ),
+    [confirmationActive, dangerous, onClick, onConfirmation, props]
   );
 
   return (
     <Fragment>
-      {loadingSpinnerPosition ? (
-        <span
-          className={cx(
-            styles.linkWrapper,
-            loadingSpinnerPosition === "right" ? styles.reverse : null
-          )}
-          hidden={props.hidden}
-        >
-          <LoadingSpinner
-            isLoading={props.isLoading}
-            isSuccess={props.isSuccess}
-            secondary={props.secondary}
-            fadeOut
-          />
-          {renderLink()}
-        </span>
-      ) : (
-        <Fragment>{renderLink()}</Fragment>
-      )}
+      <span
+        className={cx(
+          styles.linkWrapper,
+          loadingSpinnerPosition === "right" ? styles.reverse : null
+        )}
+        hidden={props.hidden}
+      >
+        {loadingSpinnerPosition && (props.isLoading || props.isSuccess) ? (
+          <Fragment>
+            <LoadingSpinner
+              isLoading={props.isLoading}
+              isSuccess={props.isSuccess}
+              secondary={props.secondary}
+              fadeOut
+            />
+            {renderLink()}
+          </Fragment>
+        ) : (
+          <Fragment>{renderLink()}</Fragment>
+        )}
+      </span>
     </Fragment>
   );
 };
