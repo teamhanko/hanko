80739fa5f3fd0e751efdec1661771ab4d6f5ae5a
Rishi Raj Jain <rjain@llnw.com>
2023-08-02 17:37:19 +0530

    fix: angular example

--- frontend/examples/angular/src/app/login/login.component.ts
+++ frontend/examples/angular/src/app/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { HankoService } from "../services/hanko.services";
-import { Router } from "@angular/router";
+import { HankoService } from '../services/hanko.services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['../app.component.css']
+  styleUrls: ['../app.component.css'],
 })
 export class LoginComponent {
   error?: Error;
