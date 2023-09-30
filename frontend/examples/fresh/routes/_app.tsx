42961d04686d869530faf43c0e6be0a63cd3c945
ryu <114303361+ryuapp@users.noreply.github.com>
2023-09-30 10:55:38 +0900

    chore: bump up fresh v1.4.3

--- frontend/examples/fresh/routes/_app.tsx
+++ frontend/examples/fresh/routes/_app.tsx
@@ -1,16 +1,19 @@
-import {AppProps} from "$fresh/server.ts";
-import {Head} from "$fresh/runtime.ts";
+import { AppProps } from "$fresh/server.ts";
 
-export default function App({Component}: AppProps) {
+export default function App({ Component }: AppProps) {
   return (
-    <>
-      <Head>
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Hanko App</title>
         <link rel="stylesheet" href="/app.css"/>
-      </Head>
+      </head>
+      <body>
       <div class="p-4 mx-auto max-w-screen-md">
-        <Component/>
-      </div>
-    </>
+        <Component />
+        </div>
+      </body>
+    </html>
   );
 }
