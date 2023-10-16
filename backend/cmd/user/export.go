e87616ed5e2a3db6f4368809d872382e0a7fa7ae
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-10-16 17:08:11 +0530

    feat(backend): add export command

--- backend/cmd/user/export.go
+++ backend/cmd/user/export.go
@@ -1,10 +1,14 @@
 package user
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/teamhanko/hanko/backend/config"
+	"github.com/teamhanko/hanko/backend/persistence"
 )
 
 func NewExportCommand() *cobra.Command {
@@ -18,7 +22,19 @@ func NewExportCommand() *cobra.Command {
 		Short: "Export users from database into a Json file",
 		Long:  ``,
 		Run: func(cmd *cobra.Command, args []string) {
-			log.Println("Exporting users...")
+			cfg, err := config.Load(&configFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			persister, err := persistence.New(cfg.Database)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = export(persister, outputFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println(fmt.Sprintf("Successfully exported users to %s", outputFile))
 		},
 	}
 
@@ -29,4 +45,38 @@ func NewExportCommand() *cobra.Command {
 		log.Println(err)
 	}
 	return cmd
+}
+
+func export(persister persistence.Persister, outFile string) error {
+	var entries []ImportOrExportEntry
+	users, err := persister.GetUserPersister().All()
+	if err != nil {
+		return fmt.Errorf("failed to get list of users: %w", err)
+	}
+	for _, user := range users {
+		var emails []ImportOrExportEmail
+		for _, email := range user.Emails {
+			emails = append(emails, ImportOrExportEmail{
+				Address:    email.Address,
+				IsPrimary:  email.IsPrimary(),
+				IsVerified: email.Verified,
+			})
+		}
+		entry := ImportOrExportEntry{
+			UserID:    user.ID.String(),
+			Emails:    emails,
+			CreatedAt: &user.CreatedAt,
+			UpdatedAt: &user.UpdatedAt,
+		}
+		entries = append(entries, entry)
+	}
+	bytes, err := json.Marshal(entries)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(outputFile, bytes, 0600)
+	if err != nil {
+		return err
+	}
+	return nil
 }
