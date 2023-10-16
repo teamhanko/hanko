5b06caca9276c56ec3665ace75ce7f2fcb280444
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-10-16 17:11:45 +0530

    fix(backend): remove todo string

--- backend/cmd/user/root.go
+++ backend/cmd/user/root.go
@@ -7,7 +7,7 @@ import (
 func NewUserCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:   "user",
-		Short: "User import/export(TODO) tools",
+		Short: "User import/export tools",
 		Long:  `Add the ability to import users into the hanko database.`,
 	}
 }
