720e2f2a1814ff38e5c04065ab4d3f09446b964a
Caio Gomes <ocaioluis@gmail.com>
2023-10-20 10:36:19 +0100

    feat(i18n): add pt-BR translation

--- frontend/elements/src/i18n/all.ts
+++ frontend/elements/src/i18n/all.ts
@@ -2,5 +2,6 @@ import { Translations } from "./translations";
 import { de } from "./de";
 import { en } from "./en";
 import { fr } from "./fr";
+import { ptBR } from "./pt-BR";
 import { zh } from "./zh";
-export const all: Translations = { de, en, fr, zh };
+export const all: Translations = { de, en, fr, ptBR, zh };
