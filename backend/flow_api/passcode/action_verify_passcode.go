c9b6cb88f9cf3db18ab67aab27e0b88eb21e04a3
Lennart Fleischmann <lennart.fleischmann@hanko.io>
2023-11-24 15:29:59 +0100

    refactor: pull passcode verification from action into service

--- backend/flow_api/passcode/action_verify_passcode.go
+++ backend/flow_api/passcode/action_verify_passcode.go
@@ -2,14 +2,12 @@ package passcode
 
 import (
 	"errors"
+	"fmt"
 	"github.com/gofrs/uuid"
 	"github.com/teamhanko/hanko/backend/flow_api/shared"
+	"github.com/teamhanko/hanko/backend/flow_api/shared/services"
 	"github.com/teamhanko/hanko/backend/flowpilot"
-	"golang.org/x/crypto/bcrypt"
-	"time"
 )
-
-var maxPasscodeTries = 3
 
 type VerifyPasscode struct {
 	shared.Action
@@ -39,53 +37,31 @@ func (a VerifyPasscode) Execute(c flowpilot.ExecutionContext) error {
 		return err
 	}
 
-	passcode, err := deps.Persister.GetPasscodePersister().Get(passcodeId)
+	err = deps.PasscodeService.VerifyPasscode(deps.Tx, passcodeId, c.Input().Get("code").String())
 	if err != nil {
-		return err
-	}
-	if passcode == nil {
-		return errors.New("passcode not found")
-	}
-
-	expirationTime := passcode.CreatedAt.Add(time.Duration(passcode.Ttl) * time.Second)
-	if expirationTime.Before(time.Now().UTC()) {
-		return c.ContinueFlowWithError(c.GetCurrentState(), flowpilot.ErrorFormDataInvalid.Wrap(errors.New("passcode is expired")))
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(passcode.Code), []byte(c.Input().Get("code").String()))
-	if err != nil {
-		passcode.TryCount += 1
-		if passcode.TryCount >= maxPasscodeTries {
-			err = deps.Persister.GetPasscodePersister().Delete(*passcode)
-			if err != nil {
-				return err
-			}
-			err = c.Stash().Delete("passcode_id")
-			if err != nil {
-				return err
-			}
-
-			return c.ContinueFlowWithError(c.GetCurrentState(), shared.ErrorPasscodeMaxAttemptsReached)
+		if errors.Is(err, services.ErrorPasscodeInvalid) ||
+			errors.Is(err, services.ErrorPasscodeNotFound) ||
+			errors.Is(err, services.ErrorPasscodeExpired) {
+			return c.ContinueFlowWithError(c.GetErrorState(), shared.ErrorPasscodeInvalid)
 		}
-		return c.ContinueFlowWithError(c.GetCurrentState(), shared.ErrorPasscodeInvalid.Wrap(err))
+
+		if errors.Is(err, services.ErrorPasscodeMaxAttemptsReached) {
+			return c.ContinueFlowWithError(c.GetErrorState(), shared.ErrorPasscodeMaxAttemptsReached)
+		}
+
+		return fmt.Errorf("failed to verify passcode: %w", err)
 	}
 
-	// !?
-	//err = c.Stash().Set("user_id", passcode.UserId)
-	//if err != nil {
-	//	return fmt.Errorf("failed to set user_id to the stash: %w", err)
-	//}
+	err = c.Stash().Delete("passcode_id")
+	if err != nil {
+		return fmt.Errorf("failed to delete passcode_id from stash: %w", err)
+	}
 
 	if !c.Stash().Get("user_id").Exists() {
 		return c.ContinueFlowWithError(c.GetErrorState(), flowpilot.ErrorOperationNotPermitted.Wrap(errors.New("account does not exist")))
 	}
 
 	err = c.Stash().Set("email_verified", true) // TODO: maybe change attribute path
-	if err != nil {
-		return err
-	}
-
-	err = deps.Persister.GetPasscodePersisterWithConnection(deps.Tx).Delete(*passcode)
 	if err != nil {
 		return err
 	}
