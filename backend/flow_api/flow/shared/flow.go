08f1627d43cccb66520c0a7cbfea8e85248864ac
Lennart Fleischmann <lennart.fleischmann@hanko.io>
2023-11-24 16:23:52 +0100

    refactor: change package structure

---
+++ backend/flow_api/flow/shared/flow.go
@@ -0,0 +1,36 @@
+package shared
+
+import (
+	"github.com/gobuffalo/pop/v6"
+	"github.com/labstack/echo/v4"
+	"github.com/teamhanko/hanko/backend/config"
+	"github.com/teamhanko/hanko/backend/flow_api/services"
+	"github.com/teamhanko/hanko/backend/flowpilot"
+	"github.com/teamhanko/hanko/backend/persistence"
+	"github.com/teamhanko/hanko/backend/session"
+)
+
+const (
+	StateSuccess flowpilot.StateName = "success"
+	StateError   flowpilot.StateName = "error"
+)
+
+const (
+	ActionBack flowpilot.ActionName = "back"
+)
+
+type Dependencies struct {
+	Cfg             config.Config
+	HttpContext     echo.Context
+	PasscodeService services.Passcode
+	WebauthnService services.WebauthnService
+	Persister       persistence.Persister
+	SessionManager  session.Manager
+	Tx              *pop.Connection
+}
+
+type Action struct{}
+
+func (a *Action) GetDeps(c flowpilot.Context) *Dependencies {
+	return c.Get("dependencies").(*Dependencies)
+}
