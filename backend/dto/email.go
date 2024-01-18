805e9377801e034f87d734ae8bfc7c9b5a4a9a06
Lennart Fleischmann <lennart.fleischmann@hanko.io>
2024-01-18 17:08:28 +0100

    feat: add json omitempty tag to identity field in email model

--- backend/dto/email.go
+++ backend/dto/email.go
@@ -10,7 +10,7 @@ type EmailResponse struct {
 	Address    string    `json:"address"`
 	IsVerified bool      `json:"is_verified"`
 	IsPrimary  bool      `json:"is_primary"`
-	Identity   *Identity `json:"identity"`
+	Identity   *Identity `json:"identity,omitempty"`
 }
 
 type EmailCreateRequest struct {
