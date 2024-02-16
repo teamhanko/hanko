ff415299e23b76d1b9be1e91587259f66f7e6360
lfleischmann <67686424+lfleischmann@users.noreply.github.com>
2024-02-16 14:53:37 +0100

    chore: bump frontend versions to v0.10.0

--- frontend/examples/fresh/islands/Login.tsx
+++ frontend/examples/fresh/islands/Login.tsx
@@ -1,7 +1,7 @@
 import { HANKO_API_URL } from "../config.ts";
 
 const code = `
-  import { register } from 'https://esm.sh/@teamhanko/hanko-elements@0.9.1';
+  import { register } from 'https://esm.sh/@teamhanko/hanko-elements@0.10.0';
 
   register('${HANKO_API_URL}', { shadow: true });
   document.addEventListener('hankoAuthSuccess', (event) => {
