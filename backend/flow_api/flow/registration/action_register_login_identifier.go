015e56b467486df1b67c98514c4bf8c84b6be36c
bjoern-m <56024829+bjoern-m@users.noreply.github.com>
2024-02-22 16:32:52 +0100

    Flow api improve handling when email is unknown (#1240)
    * feat: passcode handling when email is unknown
    * feat: new passcode templates and improved behaviour to prevent email address enumeration

--- backend/flow_api/flow/registration/action_register_login_identifier.go
+++ backend/flow_api/flow/registration/action_register_login_identifier.go
@@ -67,20 +67,6 @@ func (a RegisterLoginIdentifier) Execute(c flowpilot.ExecutionContext) error {
 		}
 	}
 
-	if email != "" {
-		// Check that email is not already taken
-		// this check is non-exhaustive as the email is not blocked here and might be created after the check here and the user creation
-		emailModel, err := deps.Persister.GetEmailPersister().FindByAddress(email)
-		if err != nil {
-			return err
-		}
-		// Do not return an error when only identifier is email and email verification is on (account enumeration protection)
-		if emailModel != nil && !(!deps.Cfg.Identifier.Username.Enabled && deps.Cfg.Identifier.Email.Verification) {
-			c.Input().SetError("email", shared.ErrorEmailAlreadyExists)
-			return c.ContinueFlowWithError(c.GetCurrentState(), flowpilot.ErrorFormDataInvalid)
-		}
-	}
-
 	if username != "" {
 		// Check that username is not already taken
 		// this check is non-exhaustive as the username is not blocked here and might be created after the check here and the user creation
@@ -91,6 +77,35 @@ func (a RegisterLoginIdentifier) Execute(c flowpilot.ExecutionContext) error {
 		if userModel != nil {
 			c.Input().SetError("username", shared.ErrorUsernameAlreadyExists)
 			return c.ContinueFlowWithError(c.GetCurrentState(), flowpilot.ErrorFormDataInvalid)
+		}
+	}
+
+	if email != "" {
+		// Check that email is not already taken
+		// this check is non-exhaustive as the email is not blocked here and might be created after the check here and the user creation
+		emailModel, err := deps.Persister.GetEmailPersister().FindByAddress(email)
+		if err != nil {
+			return err
+		}
+		// Do not return an error when only identifier is email and email verification is on (account enumeration protection)
+		if emailModel != nil {
+			// E-mail address already exists
+			if !deps.Cfg.Emails.RequireVerification {
+				c.Input().SetError("email", shared.ErrorEmailAlreadyExists)
+				return c.ContinueFlowWithError(c.GetCurrentState(), flowpilot.ErrorFormDataInvalid)
+			} else {
+				err = c.CopyInputValuesToStash("email")
+				if err != nil {
+					return fmt.Errorf("failed to copy email to stash: %w", err)
+				}
+
+				err = c.Stash().Set("passcode_template", "email_registration_attempted")
+				if err != nil {
+					return fmt.Errorf("failed to set passcode_template to the stash: %w", err)
+				}
+
+				return c.StartSubFlow(passcode.StatePasscodeConfirmation)
+			}
 		}
 	}
 
@@ -110,6 +125,7 @@ func (a RegisterLoginIdentifier) Execute(c flowpilot.ExecutionContext) error {
 	}
 
 	// Decide which is the next state according to the config and user input
+
 	if email != "" && deps.Cfg.Identifier.Email.Verification {
 		if err := c.Stash().Set("passcode_template", "email_verification"); err != nil {
 			return fmt.Errorf("failed to set passcode_template to stash: %w", err)
