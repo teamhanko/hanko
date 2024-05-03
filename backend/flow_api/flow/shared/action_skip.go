ad247e5d5cdc4ee08f4c08887e869facf4b9750b
Lennart Fleischmann <lennart.fleischmann@hanko.io>
2024-05-03 14:40:59 +0200

    wip5

--- backend/flow_api/flow/shared/action_skip.go
+++ backend/flow_api/flow/shared/action_skip.go
@@ -16,22 +16,7 @@ func (a Skip) GetDescription() string {
 	return "Skip"
 }
 
-func (a Skip) Initialize(c flowpilot.InitializationContext) {
-	//if !c.Stash().Get("skip_to").Exists() || c.Stash().Get("skip_to").String() == "" {
-	//	c.SuspendAction()
-	//}
-
-	//if !c.Stash().Get("skip_from").Exists() || c.Stash().Get("skip_from").String() == "" {
-	//	c.SuspendAction()
-	//}
-
-	//if nextState, ok := c.GetNextStateDuringInitForSchemaCreation(); ok {
-	//	if nextState != flowpilot.StateName(c.Stash().Get("skip_from").String()) {
-	//		c.SuspendAction()
-	//	}
-	//}
-
-}
+func (a Skip) Initialize(c flowpilot.InitializationContext) {}
 
 func (a Skip) Execute(c flowpilot.ExecutionContext) error {
 	return c.EndSubFlow()
