965832e758d0372feb89579b8ab7f948da03cc66
Jean-Francois Labonte <jefflabonte@protonmail.com>
2024-06-10 00:13:04 -0400

    Change of strategy devenv is not working as expected with containers

--- devenv.nix
+++ devenv.nix
@@ -11,33 +11,26 @@ in
   packages = [
     goPkgs.go_1_20
     pkgs.cacert
-  ] ++ lib.optionals ( !config.container.isBuilding) [
+    pkgs.git
     pkgs.docker
     pkgs.docker-compose
-    pkgs.git
+    pkgs.nixpacks
   ];
 
   enterShell = ''
+    export SSL_CERT_DIR=${pkgs.cacert}/etc/ssl/certs/
+
     if [[ ! -f .env ]]; then
       cp .env.template .env
       echo "Created a new .env file from .env.example"
     fi
   '';
 
-  processes.serveBackend = {
-    exec = "cd backend && ${goPkgs.go_1_20}/bin/go";
-  };
-
-  containers = {
-    "hanko" = {
-      copyToRoot = ./backend;
-      name = config.env.IMAGE_NAME;
-      startupCommand = ''
-        export SSL_CERT_DIR=${pkgs.cacert}/etc/ssl/certs/
-        ${goPkgs.go_1_20}/bin/go generate ./...
-        CGO_ENABLED=0 GOOS=linux GOARCH="$TARGETARCH" ${goPkgs.go_1_20}/bin/go build -a -o hanko main.go
-        ./hanko
-      '';
-    };
+  scripts = {
+    build_image.exec = ''
+      nixpacks build ./backend \
+        --name ${config.env.IMAGE_NAME}
+    '';
+    push_image.exec = "devenv container --registry docker:// copy hanko";
   };
 }
