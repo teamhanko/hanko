a58915efba3cc74025657675f904295f5afe2edc
bjoern-m <bjoern.mueller@hanko.io>
2024-07-01 22:42:11 +0200

    chore: split password_set action into password_create and password_update

--- backend/flow_api/flow/profile/action_password_create.go
+++ backend/flow_api/flow/profile/action_password_create.go
@@ -23,16 +23,24 @@ func (a PasswordCreate) GetDescription() string {
 func (a PasswordCreate) Initialize(c flowpilot.InitializationContext) {
 	deps := a.GetDeps(c)
 
+	userModel, _ := c.Get("session_user").(*models.User)
+
 	if !deps.Cfg.Password.Enabled {
 		c.SuspendAction()
-	} else {
-		c.AddInputs(flowpilot.StringInput("password").
-			Required(true).
-			MinLength(deps.Cfg.Password.MinLength).
-			MaxLength(72).
-			Persist(false),
-		)
 	}
+
+	if userModel.PasswordCredential != nil {
+		// The password_update action must be used instead
+		c.SuspendAction()
+	}
+
+	c.AddInputs(flowpilot.StringInput("password").
+		Required(true).
+		MinLength(deps.Cfg.Password.MinLength).
+		MaxLength(72).
+		Persist(false),
+	)
+
 }
 
 func (a PasswordCreate) Execute(c flowpilot.ExecutionContext) error {
@@ -47,20 +55,11 @@ func (a PasswordCreate) Execute(c flowpilot.ExecutionContext) error {
 		return c.Error(flowpilot.ErrorOperationNotPermitted)
 	}
 
-	passwordCredential, err := deps.Persister.GetPasswordCredentialPersisterWithConnection(deps.Tx).GetByUserID(userModel.ID)
-	if err != nil {
-		return fmt.Errorf("could not fetch password credential: %w", err)
-	}
-
 	password := c.Input().Get("password").String()
 
-	if passwordCredential == nil {
-		passwordCredential = models.NewPasswordCredential(userModel.ID, "") // ?
-		err = deps.PasswordService.CreatePassword(userModel.ID, password)
-	} else {
-		err = deps.PasswordService.UpdatePassword(passwordCredential, password)
-	}
+	passwordCredential := models.NewPasswordCredential(userModel.ID, password) // ?
 
+	err := deps.PasswordService.CreatePassword(userModel.ID, password) // ?
 	if err != nil {
 		return fmt.Errorf("could not set password: %w", err)
 	}
