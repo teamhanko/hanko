fd93bebbfcbd2b2c25f63fe4135454c477af950e
bjoern-m <bjoern.mueller@hanko.io>
2024-07-29 18:22:11 +0200

    chore: the hanko-elements can be used in nextjs without dynamic imports

--- frontend/examples/nextjs/components/HankoAuth.tsx
+++ frontend/examples/nextjs/components/HankoAuth.tsx
@@ -1,5 +1,5 @@
 import { Hanko, register } from "@teamhanko/hanko-elements";
-import { useCallback, useEffect, useMemo } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 const api = process.env.NEXT_PUBLIC_HANKO_API!;
@@ -10,17 +10,19 @@ interface Props {
 
 function HankoAuth({ setError }: Props) {
   const router = useRouter();
-  const hankoClient = useMemo(() => new Hanko(api), []);
+  const [hankoClient, setHankoClient] = useState<Hanko>();
 
   const redirectToTodos = useCallback(() => {
     router.replace("/todo").catch(setError);
   }, [router, setError]);
 
+  useEffect(() => setHankoClient(new Hanko(api)), []);
+
   useEffect(() => {
     register(api).catch(setError);
   }, [setError]);
 
-  useEffect(() => hankoClient.onAuthFlowCompleted(() => {
+  useEffect(() => hankoClient?.onAuthFlowCompleted(() => {
     redirectToTodos()
   }), [hankoClient, redirectToTodos]);
 
