0236cb39baf79db9153da8f6f770bcebfdd8897c
bjoern-m <bjoern.mueller@hanko.io>
2024-08-26 15:45:16 +0200

    chore: update versions to 1.0.1

--- frontend/examples/fresh/islands/LogoutButton.tsx
+++ frontend/examples/fresh/islands/LogoutButton.tsx
@@ -1,7 +1,7 @@
 import { HANKO_API_URL } from "../config.ts";
 
 const code = `
-  import { register, Hanko } from 'https://esm.sh/@teamhanko/hanko-elements@1.0.0';
+  import { register, Hanko } from 'https://esm.sh/@teamhanko/hanko-elements@1.0.1';
 
   register('${HANKO_API_URL}', { shadow: true });
   window.addEventListener('logout', () => {
