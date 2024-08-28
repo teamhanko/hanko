453567d45fcb576eeb3c54c0e41e6da66b70ae36
Frederic Jahn <frederic.jahn@hanko.io>
2024-08-28 14:02:05 +0200

    chore: update versions to 1.0.2 (#1599)
    * chore: update versions to 1.0.2
    
    * chore: add package-lock.json

--- frontend/examples/fresh/islands/Profile.tsx
+++ frontend/examples/fresh/islands/Profile.tsx
@@ -1,7 +1,7 @@
 import { HANKO_API_URL } from "../config.ts";
 
 const code = `
-  import { register } from 'https://esm.sh/@teamhanko/hanko-elements@1.0.1';
+  import { register } from 'https://esm.sh/@teamhanko/hanko-elements@1.0.2';
   register('${HANKO_API_URL}', { shadow: true });
 `;
 
