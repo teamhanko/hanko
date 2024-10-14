5cc0bbd8c0757a69d3cade3d21820dc800de1b6c
Frederic Jahn <frederic.jahn@hanko.io>
2024-10-14 17:10:23 +0200

    chore: add db validator to session model

--- backend/persistence/models/session.go
+++ backend/persistence/models/session.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
 	"github.com/gofrs/uuid"
 	"time"
 )
@@ -14,4 +17,14 @@ type Session struct {
 	UpdatedAt time.Time  `db:"updated_at"`
 	ExpiresAt *time.Time `db:"expires_at"`
 	LastUsed  time.Time  `db:"last_used"`
+}
+
+func (session *Session) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.UUIDIsPresent{Name: "ID", Field: session.ID},
+		&validators.UUIDIsPresent{Name: "UserID", Field: session.UserID},
+		&validators.TimeIsPresent{Name: "LastUsed", Field: session.UpdatedAt},
+		&validators.TimeIsPresent{Name: "UpdatedAt", Field: session.UpdatedAt},
+		&validators.TimeIsPresent{Name: "CreatedAt", Field: session.CreatedAt},
+	), nil
 }
