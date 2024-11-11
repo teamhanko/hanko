86b4ceae160d33efb7860a07b98a9b60a807d49e
Frederic Jahn <frederic.jahn@hanko.io>
2024-11-11 09:54:38 +0100

    chore: add validator to ip field

--- backend/dto/admin/session.go
+++ backend/dto/admin/session.go
@@ -3,7 +3,7 @@ package admin
 type CreateSessionTokenDto struct {
 	UserID    string `json:"user_id" validate:"required,uuid4"`
 	UserAgent string `json:"user_agent"`
-	IpAddress string `json:"ip_address"`
+	IpAddress string `json:"ip_address" validate:"omitempty,ip"`
 }
 
 type CreateSessionTokenResponse struct {
