dadfbde1ef89edeb44cc4cfa09d6f7caffe53b32
Frederic Jahn <frederic.jahn@hanko.io>
2024-12-09 16:48:04 +0100

    chore: typos

--- backend/cmd/user/format.go
+++ backend/cmd/user/format.go
@@ -27,9 +27,9 @@ func (ImportOrExportEmail) JSONSchemaExtend(schema *jsonschema.Schema) {
 type Emails []ImportOrExportEmail
 
 type ImportWebauthnCredential struct {
-	// ID of the webauthn credential.
+	// ID of the WebAuthn credential.
 	ID string `json:"id" yaml:"id" validate:"required"`
-	// Optional Name of the webauthn credential.
+	// Optional Name of the WebAuthn credential.
 	Name *string `json:"name" yaml:"name" validate:"omitempty"`
 	// The PublicKey of the credential.
 	PublicKey string `json:"public_key" yaml:"public_key" validate:"required"`
@@ -37,24 +37,24 @@ type ImportWebauthnCredential struct {
 	AttestationType string `json:"attestation_type" yaml:"attestation_type" validate:"required"`
 	// Optional AAGUID of the authenticator on which the credential was created on.
 	AAGUID uuid.UUID `json:"aaguid" yaml:"aaguid" validate:"omitempty,uuid4"`
-	// Optional SignCount of the webauthn credential.
+	// Optional SignCount of the WebAuthn credential.
 	SignCount int `json:"sign_count" yaml:"sign_count"`
-	// LastUsedAt optional timestamp when the webauthn credential was last used.
+	// LastUsedAt optional timestamp when the WebAuthn credential was last used.
 	LastUsedAt *time.Time `json:"last_used_at" yaml:"last_used_at" validate:"omitempty"`
-	// CreatedAt optional timestamp of the webauthn credentials' creation. Will be set to the import date if not provided.
+	// CreatedAt optional timestamp of the WebAuthn credentials' creation. Will be set to the import date if not provided.
 	CreatedAt *time.Time `json:"created_at" yaml:"created_at" validate:"omitempty"`
-	// UpdatedAt optional timestamp of the last update to the webauthn credential. Will be set to the import date if not provided.
+	// UpdatedAt optional timestamp of the last update to the WebAuthn credential. Will be set to the import date if not provided.
 	UpdatedAt *time.Time `json:"updated_at" yaml:"updated_at" validate:"omitempty"`
 	// Optional list of supported Transports by the authenticator.
 	Transports []string `json:"transports" yaml:"transports" validate:"omitempty,unique"`
-	// BackupEligible flag indicates if the webauthn credential can be backed up (e.g. in Apple KeyChain, ...). If the information is not available set it to false.
+	// BackupEligible flag indicates if the WebAuthn credential can be backed up (e.g. in Apple KeyChain, ...). If the information is not available set it to false.
 	BackupEligible bool `json:"backup_eligible" yaml:"backup_eligible"`
-	// BackupState flag indicates if the webauthn credential is backed up (e.g. in Apple KeyChain, ...). If the information is not available set it to false.
+	// BackupState flag indicates if the WebAuthn credential is backed up (e.g. in Apple KeyChain, ...). If the information is not available set it to false.
 	BackupState bool `json:"backup_state" yaml:"backup_state"`
-	// MFAOnly flag indicates if the webauthn credential can only be used in combination with another login factor (e.g. password, ...).
+	// MFAOnly flag indicates if the WebAuthn credential can only be used in combination with another login factor (e.g. password, ...).
 	MFAOnly bool `json:"mfa_only" yaml:"mfa_only"`
 	// UserHandle optional user id which was used to create the credential with.
-	// Populate only when user id was not an uuid v4 and the webauthn credential is not an MFAOnly credential.
+	// Populate only when user id was not an uuid v4 and the WebAuthn credential is not an MFAOnly credential.
 	UserHandle *string `json:"user_handle" yaml:"user_handle" validate:"omitempty,excluded_if=MFAOnly true"`
 }
 
@@ -86,7 +86,7 @@ type ImportOrExportEntry struct {
 	Emails Emails `json:"emails" yaml:"emails" jsonschema:"type=array,minItems=1" validate:"required_if=Username 0,unique=Address,dive"`
 	// Username optional username of the user
 	Username *string `json:"username,omitempty" yaml:"username" validate:"required_if=Emails 0,omitempty,gte=1"`
-	// WebauthnCredentials optional list of webauthn credentials of a user. Includes passkeys and MFA credentials.
+	// WebauthnCredentials optional list of WebAuthn credentials of a user. Includes passkeys and MFA credentials.
 	WebauthnCredentials ImportWebauthnCredentials `json:"webauthn_credentials,omitempty" yaml:"webauthn_credentials" validate:"omitempty,unique=ID,dive"`
 	// Password optional password.
 	Password *ImportPasswordCredential `json:"password" yaml:"password" validate:"omitempty"`
