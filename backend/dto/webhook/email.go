e93788d7744b83fee9458713ac83891c65770736
Lennart Fleischmann <67686424+lfleischmann@users.noreply.github.com>
2025-02-11 19:07:46 +0100

    feat: use exact template names for email.send webhook types

--- backend/dto/webhook/email.go
+++ backend/dto/webhook/email.go
@@ -1,14 +1,14 @@
 package webhook
 
 type EmailSend struct {
-	Subject          string    `json:"subject"`        // subject
-	BodyPlain        string    `json:"body_plain"`     // used for string templates
-	Body             string    `json:"body,omitempty"` // used for HTML templates
-	ToEmailAddress   string    `json:"to_email_address"`
-	DeliveredByHanko bool      `json:"delivered_by_hanko"`
-	AcceptLanguage   string    `json:"accept_language"` // Deprecated. Accept-Language header from HTTP request
-	Language         string    `json:"language"`        // X-Language header from HTTP request
-	Type             EmailType `json:"type"`            // type of the email, currently only "passcode", but other could be added later
+	Subject          string `json:"subject"`        // subject
+	BodyPlain        string `json:"body_plain"`     // used for string templates
+	Body             string `json:"body,omitempty"` // used for HTML templates
+	ToEmailAddress   string `json:"to_email_address"`
+	DeliveredByHanko bool   `json:"delivered_by_hanko"`
+	AcceptLanguage   string `json:"accept_language"` // Deprecated. Accept-Language header from HTTP request
+	Language         string `json:"language"`        // X-Language header from HTTP request
+	Type             string `json:"type"`            // type of the email
 
 	Data interface{} `json:"data"`
 }
@@ -19,9 +19,3 @@ type PasscodeData struct {
 	TTL         int    `json:"ttl"`
 	ValidUntil  int64  `json:"valid_until"` // UnixTimestamp
 }
-
-type EmailType string
-
-var (
-	EmailTypePasscode EmailType = "passcode"
-)
