b70fb8e921b20bedcdff95ceb43877655cb672da
Frederic Jahn <frederic.jahn@hanko.io>
2025-11-13 11:33:14 +0100

    chore: increase version numbers

--- frontend/examples/fresh/islands/Profile.tsx
+++ frontend/examples/fresh/islands/Profile.tsx
@@ -1,7 +1,7 @@
 import { HANKO_API_URL } from "../config.ts";
 
 const code = `
-  import { register } from 'https://esm.sh/@teamhanko/hanko-elements@2.2.0';
+  import { register } from 'https://esm.sh/@teamhanko/hanko-elements@2.2.1';
   register('${HANKO_API_URL}', { shadow: true });
 `;
 
