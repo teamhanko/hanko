e416ce8b435ed06de22e9888931c95b353e179b8
Matthew H. Irby <matt.irby@outlook.com>
2025-12-03 23:05:18 -0500

    feat(security_notifications): mfa disabled notifications
    Signed-off-by: Matthew H. Irby <matt.irby@outlook.com>

--- backend/flow_api/flow/profile/action_otp_secret_delete.go
+++ backend/flow_api/flow/profile/action_otp_secret_delete.go
@@ -2,7 +2,9 @@ package profile
 
 import (
 	"fmt"
+
 	"github.com/teamhanko/hanko/backend/v2/flow_api/flow/shared"
+	"github.com/teamhanko/hanko/backend/v2/flow_api/services"
 	"github.com/teamhanko/hanko/backend/v2/flowpilot"
 	"github.com/teamhanko/hanko/backend/v2/persistence/models"
 )
@@ -53,6 +55,26 @@ func (a OTPSecretDelete) Execute(c flowpilot.ExecutionContext) error {
 	}
 
 	userModel.DeleteOTPSecret()
+
+	var hasOtherMfa bool = false
+
+	for _, credential := range userModel.WebauthnCredentials {
+		if credential.MFAOnly {
+			// User has another MFA-only credential
+			hasOtherMfa = true
+			break
+		}
+	}
+
+	// Send MFA disabled notification if there are no more MFA methods
+	if !hasOtherMfa && userModel.OTPSecret == nil && deps.Cfg.SecurityNotifications.Notifications.MFADisabled.Enabled {
+		deps.SecurityNotificationService.SendNotification(deps.Tx, services.SendSecurityNotificationParams{
+			EmailAddress: userModel.Emails.GetPrimary().Address,
+			Template:     "mfa_disabled",
+			HttpContext:  deps.HttpContext,
+			UserContext:  *userModel,
+		})
+	}
 
 	return c.Continue(shared.StateProfileInit)
 }
